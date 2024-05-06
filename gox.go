c88e88ecc67e4339109ea097bc881f696a5dc6a8
donnie4w <donnie4w@gmail.com>
2024-05-06 23:00:58 +0800

    Update gox.go

--- gox.go
+++ gox.go
@@ -10,18 +10,19 @@ import (
 
 func main() {
 	logging.SetFormat(logging.FORMAT_DATE | logging.FORMAT_TIME)
-	port, nolog, https := 8080, false, false
+	port, nolog, https, root := 8080, false, false, "./"
 	flag.IntVar(&port, "port", 8080, "Listen port")
 	flag.BoolVar(&nolog, "nolog", false, "not print log")
 	flag.BoolVar(&https, "https", false, "use https")
+	flag.StringVar(&root, "root", "./", "root path")
 	flag.Parse()
 	tl := tlnet.NewTlnet()
-	tl.HandleStatic("/", "./", func(hc *tlnet.HttpContext) {
+	tl.HandleStatic("/", root, func(hc *tlnet.HttpContext) {
 		if !nolog {
 			logging.Debug(hc.Request().RequestURI)
 		}
 	})
-	logging.Info("Gox Start and params:", "port[", port, "]", "nolog[", nolog, "]", "https[", https, "]")
+	logging.Info("Gox Start and params:", "port[", port, "]", "nolog[", nolog, "]", "https[", https, "]", "root[", root, "]")
 	var err error
 	if https {
 		err = tl.HttpStartTlsBytes(fmt.Sprint(":", port), []byte(keystore.ServerCrt), []byte(keystore.ServerKey))
